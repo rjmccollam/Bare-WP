6cad9a334e3c434a055fcbd22246502e31c4a869
RJ McCollam <rj@wedodev.com>
2014-03-23 12:49:19 -0700

    Updated 404 and README
    A more useful 404 that includes search and link back to home page
    
    Signed-off-by: RJ McCollam <rj@wedodev.com>

--- 404.php
+++ 404.php
@@ -1,6 +1,12 @@
 <?php get_header(); ?>
 
-	<h1>Error 404 - Page Not Found</h1>
+	<h1>Whoops! 404</h1>
+
+	<p>We're sorry, we couldn't find the page that you were looking for.</p>
+
+	<h2>You can try searching for something else below, or you could return to our <a href="<?php bloginfo('url'); ?>">home page</a></h2>
+
+	<?php get_search_form(); ?>
 
 <?php get_sidebar(); ?>
 
