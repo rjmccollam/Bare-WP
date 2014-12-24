a7354f0dbbda1ee7065c70067ada5eb5e5c7e1ea
RJ McCollam <rj@wedodev.com>
2014-12-24 10:00:22 -0600

    added title-tag theme support
    Signed-off-by: RJ McCollam <rj@wedodev.com>

--- functions.php
+++ functions.php
@@ -31,6 +31,18 @@
             }
         }
     }
+
+    // Add title tag to <head> section
+    add_theme_support( 'title-tag' );
+
+    if ( ! function_exists( '_wp_render_title_tag' ) ) :
+        function theme_slug_render_title() {
+    ?>
+    <title><?php wp_title( '|', true, 'right' ); ?></title>
+    <?php
+        }
+        add_action( 'wp_head', 'theme_slug_render_title' );
+    endif;
 	
 	// Add RSS links to <head> section
 	add_theme_support( 'automatic-feed-links' );
--- header.php
+++ header.php
@@ -6,27 +6,6 @@
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>">
 
 	<meta name="viewport" content="width=device-width">
-
-	<title>
-		   <?php
-		      if (function_exists('is_tag') && is_tag()) {
-		         single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
-		      elseif (is_archive()) {
-		         wp_title(''); echo ' Archive - '; }
-		      elseif (is_search()) {
-		         echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
-		      elseif (!(is_404()) && (is_single()) || (is_page())) {
-		         wp_title(''); echo ' - '; }
-		      elseif (is_404()) {
-		         echo 'Not Found - '; }
-		      if (is_home() || is_front_page()) {
-		         bloginfo('name'); echo ' - '; bloginfo('description'); }
-		      else {
-		          bloginfo('name'); }
-		      if ($paged>1) {
-		         echo ' - page '. $paged; }
-		   ?>
-	</title>
 		
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
 	
