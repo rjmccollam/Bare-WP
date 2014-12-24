c078807021d0248aee778ba7f9b19fa54383a516
RJ McCollam <rj@wedodev.com>
2014-12-24 10:11:39 -0600

    added label to comment field
    Signed-off-by: RJ McCollam <rj@wedodev.com>

--- comments.php
+++ comments.php
@@ -82,6 +82,7 @@
 		<!--<p>You can use these tags: <code><?php echo allowed_tags(); ?></code></p>-->
 
 		<div>
+			<label for="comment">Comment</label>
 			<textarea name="comment" id="comment" cols="58" rows="10" tabindex="4"></textarea>
 		</div>
 
