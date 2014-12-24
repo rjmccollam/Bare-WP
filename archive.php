59384bc5b5e27de3f79ec1aa18032ba0cf56dc20
RJ McCollam <rj@wedodev.com>
2014-12-24 10:10:27 -0600

    cleaned up archive <h1> conditionals
    Signed-off-by: RJ McCollam <rj@wedodev.com>

--- archive.php
+++ archive.php
@@ -4,28 +4,23 @@
 
  			<?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
 
-			<?php /* If this is a category archive */ if (is_category()) { ?>
-				<h1>Archive for the &#8216;<?php single_cat_title(); ?>&#8217; Category</h1>
+ 			<h1>
 
-			<?php /* If this is a tag archive */ } elseif( is_tag() ) { ?>
-				<h1>Posts Tagged &#8216;<?php single_tag_title(); ?>&#8217;</h1>
+				<?php if (is_category()) { ?>Archive for the &#8216;<?php single_cat_title(); ?>&#8217; Category
 
-			<?php /* If this is a daily archive */ } elseif (is_day()) { ?>
-				<h1>Archive for <?php the_time('F jS, Y'); ?></h1>
+				<?php } elseif( is_tag() ) { ?>Posts Tagged &#8216;<?php single_tag_title(); ?>&#8217;
 
-			<?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
-				<h1>Archive for <?php the_time('F, Y'); ?></h1>
+				<?php } elseif (is_day()) { ?>Archive for <?php the_time('F jS, Y'); ?>
 
-			<?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
-				<h1>Archive for <?php the_time('Y'); ?></h1>
+				<?php } elseif (is_month()) { ?>Archive for <?php the_time('F, Y'); ?>
 
-			<?php /* If this is an author archive */ } elseif (is_author()) { ?>
-				<h1>Author Archive</h1>
+				<?php } elseif (is_year()) { ?>Archive for <?php the_time('Y'); ?>
 
-			<?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-				<h1>Blog Archives</h1>
-			
-			<?php } ?>
+				<?php } elseif (is_author()) { ?>Author Archive
+
+				<?php } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>Blog Archives<?php } ?>
+
+			</h1>
 
 			<?php while (have_posts()) : the_post(); ?>
 			
