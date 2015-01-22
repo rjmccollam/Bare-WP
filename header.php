464290d5d85ef55d899f6c0c1dc23e2f98134538
RJ McCollam <rj@wedodev.com>
2015-01-22 09:47:50 -0600

    Removed duplicate stylesheet tag in the <head>

--- header.php
+++ header.php
@@ -6,8 +6,6 @@
 	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>">
 
 	<meta name="viewport" content="width=device-width">
-		
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
 	
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
